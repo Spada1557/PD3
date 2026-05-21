86a9e3a36a308b2f45efa4b0d700a5e2939125cd
Fundware <54841686+Spada1557@users.noreply.github.com>
2026-05-21 18:28:12 +0700

    Delete deepseek directory

--- deepseek/start-backend.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/group.s7/m.goloshchanov/IdeaProjects/SkillsTest/deepseek/backend
-rm -f db/warehouse.db
-exec /usr/bin/python3.10 -m uvicorn app.main:app --host 0.0.0.0 --port 8000
